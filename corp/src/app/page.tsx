d214dab07be806a003a1e8ae0bf53ebc1773215a
luyvannda <philips.joe329@gmail.com>
2024-01-20 16:01:00 +0700

    learn to use Image component and fill props

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,5 +1,18 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "/public/home.jpg";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-white">Home Page</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="Home Page Image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
