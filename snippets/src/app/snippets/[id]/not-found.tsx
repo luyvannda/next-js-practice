bc1823d73359194aba1350c73e7f533faf984aae
luyvannda <luyvannda@gmail.com>
2024-02-09 15:53:27 +0700

    learn to customize not found page

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl bold">
+        404 | sorry but we couldn&apos;t find that particular snippet
+      </h1>
+    </div>
+  );
+}
