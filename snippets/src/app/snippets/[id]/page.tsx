6de3ebd324ed758d4878b3fa8218c8b0c580d3f3
luyvannda <luyvannda@gmail.com>
2024-02-09 16:02:09 +0700

    learn to add a loading page

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -8,6 +8,9 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  // This line of code intent to test the loading page by setting a delay by 2 seconds
+  await new Promise((r) => setTimeout(r, 2000));
+
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) }
   });
