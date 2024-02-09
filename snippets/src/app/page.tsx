fc41674805ebc8437dea14fe80b42ad1844879ac
luyvannda <luyvannda@gmail.com>
2024-02-09 16:28:10 +0700

    add link to home page to navigate to each snippets as wel as the create new snippet page

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,11 +1,34 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        className="flex justify-between items-center p-2 border border-slate-500 rounded"
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex justify-between m-2 items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link
+          className="border p-2 rounded border-slate-500"
+          href="/snippets/new"
+        >
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
